3a173f1a83cf0e5118f69db182dd6e19b7d09d47
Fernando Santos <santiles87@gmail.com>
2023-07-06 12:43:35 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+function toggleMode() {
+  const html = document.documentElement
+  
+  /*if(html.classlist.contains('light')) {
+    html.classList.remove('light') 
+  } else {   
+    html.classList.add('light')
+  }*/
+
+  // pegar a tag img 
+
+  // substituir a imagem
+  
+  // se tiver light mode, adicionar a img light
+  
+  // se tiver sem light mode, manter a img normal
+  html.classList.toggle('light')
+}
